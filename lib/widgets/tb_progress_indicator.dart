28a825510ec9259bd354a3dafdb61d49f6c8e2cd
Tristan Gonzalez Branca <trgonzalez@itba.edu.ar>
2026-03-18 00:22:36 -0300

    branding: replace TB spinner with standard CircularProgressIndicator

--- lib/widgets/tb_progress_indicator.dart
+++ lib/widgets/tb_progress_indicator.dart
@@ -1,8 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:thingsboard_app/constants/assets_path.dart';
 
 class TbProgressIndicator extends ProgressIndicator {
 
@@ -24,67 +20,18 @@ class TbProgressIndicator extends ProgressIndicator {
       valueColor?.value ?? Theme.of(context).primaryColor;
 }
 
-class _TbProgressIndicatorState extends State<TbProgressIndicator>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late CurvedAnimation _rotation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
-      vsync: this,
-      animationBehavior: AnimationBehavior.preserve,
-    );
-    _rotation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
-    _controller.repeat();
-  }
-
-  @override
-  void didUpdateWidget(TbProgressIndicator oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (!_controller.isAnimating) _controller.repeat();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _TbProgressIndicatorState extends State<TbProgressIndicator> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SvgPicture.asset(
-          ThingsboardImage.thingsboardCenter,
-          height: widget.size,
-          width: widget.size,
-          colorFilter: ColorFilter.mode(
-            widget._getValueColor(context),
-            BlendMode.srcIn,
-          ),
+    return SizedBox(
+      width: widget.size,
+      height: widget.size,
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(
+          widget._getValueColor(context),
         ),
-        AnimatedBuilder(
-          animation: _rotation,
-          child: SvgPicture.asset(
-            ThingsboardImage.thingsboardOuter,
-            height: widget.size,
-            width: widget.size,
-            colorFilter: ColorFilter.mode(
-              widget._getValueColor(context),
-              BlendMode.srcIn,
-            ),
-          ),
-          builder: (BuildContext context, Widget? child) {
-            return Transform.rotate(
-              angle: _rotation.value * pi * 2,
-              child: child,
-            );
-          },
-        ),
-      ],
+        strokeWidth: 3.0,
+      ),
     );
   }
 }
